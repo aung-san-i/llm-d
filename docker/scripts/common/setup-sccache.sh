b3646cde40e2a63aefbd2d567ff822eb6e7faa58
Tyler Michael Smith <tlrmchlsmth@gmail.com>
2026-01-29 17:40:20 -0500

    More sccache tweaks
    Signed-off-by: Tyler Michael Smith <tlrmchlsmth@gmail.com>

--- docker/scripts/common/setup-sccache.sh
+++ docker/scripts/common/setup-sccache.sh
@@ -36,6 +36,8 @@ if [ "${USE_SCCACHE}" = "true" ]; then
     if ! /usr/local/bin/sccache --start-server; then
         echo "Warning: sccache failed to start, continuing without cache" >&2
         unset CMAKE_C_COMPILER_LAUNCHER CMAKE_CXX_COMPILER_LAUNCHER CMAKE_CUDA_COMPILER_LAUNCHER
+        # Remove sccache binary so meson/cmake can't accidentally try to use it
+        rm -f /usr/local/bin/sccache
         return 0
     fi
 
@@ -43,6 +45,8 @@ if [ "${USE_SCCACHE}" = "true" ]; then
         echo "Warning: sccache not responding properly, disabling cache" >&2
         /usr/local/bin/sccache --stop-server 2>/dev/null || true
         unset CMAKE_C_COMPILER_LAUNCHER CMAKE_CXX_COMPILER_LAUNCHER CMAKE_CUDA_COMPILER_LAUNCHER
+        # Remove sccache binary so meson/cmake can't accidentally try to use it
+        rm -f /usr/local/bin/sccache
         return 0
     fi
 
